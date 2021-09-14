6f0921de2441b004d6f0452e068efed983ff618e
arnas <arnas.sleivys@visma.com>
2021-09-14 22:13:00 +0300

    Initial project structure

---
+++ MiniBankingApp/MiniBankingAppTests/MiniBankingAppTests.swift
@@ -0,0 +1,33 @@
+//
+//  MiniBankingAppTests.swift
+//  MiniBankingAppTests
+//
+//  Created by Arnas Sleivys on 2021-09-14.
+//
+
+import XCTest
+@testable import MiniBankingApp
+
+class MiniBankingAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
